f1448bd737175393ba51a9f9eb6b964706271624
Muhammadulloh <muhammadullohturayev@gmail.com>
2024-01-16 17:29:36 +0500

    larvel5

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => "Admin",
+            'role_id' => 1,
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin')
+        ]);
+    }
+}
